f8260b3fb38d0478d96b9eb48e0082aca3a57d81
robertgasp <silent.jaguar@gmail.com>
2022-03-18 00:17:46 +0300

    Lesson 3 Robolectric

--- app/src/test/java/com/example/githubusers/MainActivityTest.kt
+++ app/src/test/java/com/example/githubusers/MainActivityTest.kt
@@ -2,14 +2,17 @@ package com.example.githubusers
 
 import android.os.Build
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.githubusers.view.search.MainActivity
+import com.nhaarman.mockito_kotlin.refEq
 import junit.framework.TestCase
 import org.junit.After
 import org.junit.Before
@@ -51,10 +54,76 @@ class MainActivityTest {
     }
 
     @Test
-    fun progressBar_AreInvisible(){
+    fun progressBar_AreInvisible() {
         scenario.onActivity {
             val progressBar = it.findViewById<ProgressBar>(R.id.progressBar)
             TestCase.assertEquals(View.GONE, progressBar.visibility)
+        }
+    }
+
+    @Test
+    fun mainActivitySearchEditText_NotNull() {
+        scenario.onActivity {
+            val editText = it.findViewById<EditText>(R.id.searchEditText)
+            TestCase.assertNotNull(editText)
+        }
+    }
+
+    @Test
+    fun mainActivityToDetailsActivityButton_NotNull() {
+        scenario.onActivity {
+            val toDetailsActivityButton = it.findViewById<Button>(R.id.toDetailsActivityButton)
+            TestCase.assertNotNull(toDetailsActivityButton)
+        }
+    }
+
+
+    @Test // Проверка наличия текста в поле text на элементе "toDetailsActivityButton"
+    fun mainActivityToDetailsActivityButton_HasText() {
+        scenario.onActivity {
+            val toDetailsActivityButton = it.findViewById<Button>(R.id.toDetailsActivityButton)
+            TestCase.assertEquals("to details", toDetailsActivityButton.text)
+        }
+    }
+
+    @Test
+    fun mainActivityProgressBar_NotNull() {
+        scenario.onActivity {
+            val progressBar = it.findViewById<ProgressBar>(R.id.progressBar)
+            TestCase.assertNotNull(progressBar)
+        }
+    }
+
+
+    @Test // Проверка наличия текста в поле hint на элементе "searchEditText"
+    fun mainActivityCheckHintInEditText() {
+        scenario.onActivity {
+            val editText = it.findViewById<EditText>(R.id.searchEditText)
+            TestCase.assertEquals("Enter keyword e.g. android", editText.hint)
+        }
+    }
+
+    @Test
+    fun mainActivityEdittext_IsVisible() {
+        scenario.onActivity {
+            val editText = it.findViewById<EditText>(R.id.searchEditText)
+            TestCase.assertEquals(View.VISIBLE, editText.visibility)
+        }
+    }
+
+    @Test
+    fun mainActivityToDetailsActivityButton_IsVisible() {
+        scenario.onActivity {
+            val toDetailsActivityButton = it.findViewById<Button>(R.id.toDetailsActivityButton)
+            TestCase.assertEquals(View.VISIBLE, toDetailsActivityButton.visibility)
+        }
+    }
+
+    @Test
+    fun mainActivityRecyclerView_IsVisible() {
+        scenario.onActivity {
+            val recyclerView = it.findViewById<RecyclerView>(R.id.recyclerView)
+            TestCase.assertEquals(View.VISIBLE, recyclerView.visibility)
         }
     }
 
