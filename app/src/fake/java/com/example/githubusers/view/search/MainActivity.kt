48b3fd7ffe95bf73ebebcfeb3dec9e529dca3323
robert <silent.jaguar@gmail.com>
2022-03-23 21:26:34 +0300

    Lesson 4 Espresso

---
+++ app/src/fake/java/com/example/githubusers/view/search/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.githubusers.view.search
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.BinderThread
+import com.example.githubusers.BuildConfig
+import com.example.githubusers.R
+import com.example.githubusers.databinding.ActivityMainBinding
+import com.example.githubusers.model.SearchResult
+import com.example.githubusers.presenter.RepositoryContract
+import com.example.githubusers.presenter.search.PresenterSearchContract
+import com.example.githubusers.presenter.search.SearchPresenter
+import com.example.githubusers.repository.FakeGitHubRepository
+import com.example.githubusers.repository.GitHubApi
+import com.example.githubusers.repository.GitHubRepository
+import com.example.githubusers.view.details.DetailsActivity
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity(), ViewSearchContract {
+
+    private val adapter = SearchResultAdapter()
+    private val presenter: PresenterSearchContract = SearchPresenter(createRepository())
+    private var totalCount: Int = 0
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        presenter.onAttach(this)
+        setUI()
+    }
+
+    override fun onDestroy() {
+        presenter.onDetach(this)
+        super.onDestroy()
+    }
+
+    private fun setUI() = with(binding) {
+        toDetailsActivityButton.setOnClickListener {
+            startActivity(DetailsActivity.getIntent(this@MainActivity, totalCount))
+        }
+        setQueryListener()
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() = with(binding) {
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+    }
+
+    private fun setQueryListener() = with(binding) {
+        searchEditText.setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val query = searchEditText.text.toString()
+                if (query.isNotBlank()) {
+                    presenter.searchGitHub(query)
+                    return@OnEditorActionListener true
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.enter_search_word),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@OnEditorActionListener false
+                }
+            }
+            false
+        })
+    }
+
+    private fun createRepository(): RepositoryContract {
+            return FakeGitHubRepository()
+    }
+
+
+//    private fun createRepository(): GitHubRepository {
+//        return GitHubRepository(createRetrofit().create(GitHubApi::class.java))
+//    }
+
+    private fun createRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    override fun displaySearchResults(
+        searchResults: List<SearchResult>,
+        totalCount: Int
+    ) {
+        this.totalCount = totalCount
+        adapter.updateResults(searchResults)
+    }
+
+    override fun displayError() {
+        Toast.makeText(this, getString(R.string.undefined_error), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun displayError(error: String) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun displayLoading(show: Boolean) = with(binding) {
+        if (show) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+        }
+    }
+
+    companion object {
+        const val BASE_URL = "https://api.github.com"
+        const val FAKE = "FAKE"
+    }
+}
